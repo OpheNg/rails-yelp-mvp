fd62e2f71560c70d326f76d1b02adbebf5aaf45f
OpheNg <ophe.ng@live.fr>
2022-07-19 20:44:24 +0200

    views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+
+  root to:'restaurants#index'
+  resources :restaurants, except: [:edit, :update, :destroy] do
+    resources :reviews, only: [:new, :create, :show]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  { name: 'Le Chat noir', address: 'rue du Rouet', category: 'french', phone_number: '0491756565' },
+  { name: 'La Trattoria', address: 'Bd de la république', category: 'italian', phone_number: '0492859944' },
+  { name: 'Ramen', address: 'Lyon', category: 'japanese', phone_number: '0599004422' },
+  { name: 'Chez Wang', address: 'Paris', category: 'chinese', phone_number: '0175652233' },
+  { name: 'La frite', address: 'Bruxelle', category: 'belgian', phone_number: '0345678900' }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
